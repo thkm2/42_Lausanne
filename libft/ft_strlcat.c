0ab24dca48707c8f4d119281640d2036739d9c7a
Kim <thkm.grd@gmail.com>
2024-09-29 17:23:54 +0200

    changement des header et strlcat fait

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -1,53 +1,56 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kgiraud <kgiraud@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/29 14:52:00 by kgiraud           #+#    #+#             */
-/*   Updated: 2024/09/29 14:52:04 by kgiraud          ###   ########.fr       */
+/*   Created: 2024/09/29 14:49:15 by kgiraud           #+#    #+#             */
+/*   Updated: 2024/09/29 17:22:48 by kgiraud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *restrict dst, const char *restrict src, size_t dstsize)
+size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
 {
 	size_t	i;
-	size_t	src_len;
+	size_t	j;
+	size_t	ret;
 
 	i = 0;
-	src_len = 0;
-	while (src[src_len])
-		src_len++;
-	if (dstsize == 0)
-		return (src_len);
-	while (src[i] && i < dstsize - 1)
-	{
-		dst[i] = src[i];
+	j = 0;
+	while (src[i])
 		i++;
-	}
-	dst[i] = '\0';
-	return (src_len);
+	while (dst[j] && j < dstsize)
+		j++;
+	ret = i + j;
+	i = 0;
+	if (dstsize == 0)
+		return (ret);
+	if (j == dstsize)
+		return (ret);
+	while (j < dstsize - 1 && src[i])
+		dst[j++] = src[i++];
+	if (j < dstsize)
+		dst[j] = '\0';
+	return (ret);
 }
 
 /*#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-int	main(int ac, char **av)
+int main(void)
 {
-	if (ac != 3)
-		return (0);
-	char *dst = av[1];
-	char *src = av[2];
-	ft_strlcpy(dst, src, strlen(dst) + 1);
+	size_t dstsize = 11;
+	char dst[11] = "salut";
+	char src[] = ", mec";
+	ft_strlcat(dst, src, dstsize);
 	printf("%s \n", dst);
 
-	char *dst2 = av[1];
-	char *src2 = av[2];
-	strlcpy(dst2, src2, strlen(dst) + 1);
+	char dst2[11] = "salut";
+	strlcat(dst2, src, dstsize);
 	printf("%s \n", dst2);
-
 	return (0);
 }*/
