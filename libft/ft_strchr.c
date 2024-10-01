33ebcf3db1872679e2f0ee79e3f5c1dcd39eb91a
Kim <thkm.grd@gmail.com>
2024-10-01 19:34:45 +0200

    strchr fait

---
+++ libft/ft_strchr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kgiraud <kgiraud@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/01 19:14:18 by kgiraud           #+#    #+#             */
+/*   Updated: 2024/10/01 19:34:06 by kgiraud          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return ((char *)s + i);
+		i++;
+	}
+	if (c == '\0')
+		return ((char *)s + i);
+	return (0);
+}
+
+/*#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int ac, char **av)
+{
+	if (ac != 3)
+		return (0);
+	printf("ma fonction : %s \n", ft_strchr(av[1], atoi(av[2])));
+	printf("la vraie : %s \n", strchr(av[1], atoi(av[2])));
+	return (0);
+}*/
