3e8233190a11b51c6148c7089912416d64f4f8e3
Kim <thkm.grd@gmail.com>
2024-10-06 21:55:00 +0200

    lstmap OK avec les test

--- libft/ft_lstmap_bonus.c
+++ libft/ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: kgiraud <kgiraud@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 15:02:31 by kgiraud           #+#    #+#             */
-/*   Updated: 2024/10/05 15:31:10 by kgiraud          ###   ########.fr       */
+/*   Updated: 2024/10/06 21:53:52 by kgiraud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,16 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*tmp;
 	t_list	*new_list;
+	void	*content;
 
 	new_list = NULL;
 	while (lst)
 	{
-		tmp = ft_lstnew(f(lst->content));
+		content = f(lst->content);
+		tmp = ft_lstnew(content);
 		if (!tmp)
 		{
+			del(content);
 			ft_lstclear(&new_list, del);
 			return (NULL);
 		}
